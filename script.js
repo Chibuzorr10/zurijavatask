c12ce994034430e6b54a39fd8782aeed3930c563
Chibuzorr10 <101595121+Chibuzorr10@users.noreply.github.com>
2022-06-25 08:39:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const num1 = parseFloat(prompt("enter your first digit"));
+const operator = prompt("Enter your sign(+ , -, / , * )" );
+const num2 = parseFloat(prompt("enter your second digit"));
+
+// let result;
+
+if (operator == '+') {
+    result = num1 + num2;
+} else if (operator == '-') {
+    result = num1 - num2;
+} else if (operator == '*') {
+    result = num1 * num2;
+} else {
+    result = num1 / num2;
+}
+
+
+alert(result);
